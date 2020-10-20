c1cedd6faaf12dd9c4a038a6cf6015c6d8e90793
Cydnee Owens <cks410@yahoo.com>
2020-10-20 09:11:19 -0600

    Add initial method apartment

---
+++ lib/apartment.rb
@@ -0,0 +1,10 @@
+class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+
+  def initialize(attributes)
+    @number = attributes[:number]
+    @monthly_rent = attributes[:monthly_rent]
+    @bathrooms = attributes[:bathrooms]
+    @bedrooms = attributes[:bedrooms]
+  end
+end
