f1ff88f99de01fd36f180a4db0a5db573dcffebb
Cydnee Owens <cks410@yahoo.com>
2020-10-20 09:14:38 -0600

    Add add renter and renter method/test

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -11,6 +11,7 @@ class ApartmentTest < Minitest::Test
                             bathrooms: 1,
                             bedrooms: 1
                             })
+    @renter1 = Renter.new("Jessie")
   end
 
   def test_it_exists_and_has_attributes
@@ -19,5 +20,11 @@ class ApartmentTest < Minitest::Test
     assert_equal 1200, @unit1.monthly_rent
     assert_equal 1, @unit1.bathrooms
     assert_equal 1, @unit1.bedrooms
+  end
+
+  def test_it_can_add_a_renter
+    assert_nil @unit1.renter
+    @unit1.add_renter(@renter1)
+    assert_equal @renter1, @unit1.renter
   end
 end
